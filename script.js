34397d4ae4df349e69fe7b1e74bf81aa875990c6
guillaume-giannantonio <guillaume.giannantonio@protonmail.com>
2023-07-25 19:21:29 +0200

    add console version

--- script.js
+++ script.js
@@ -3,11 +3,29 @@ const locationInput = document.querySelector('input')
 
 searchButton.addEventListener('click', async () => {
 	const location = locationInput.value
-	const weatherData = await loadWeatherData(location)
+	const weatherData = await loadWeatherData(location.toLowerCase())
 	console.log(weatherData)
 })
 
 async function loadWeatherData(location) {
 	const response = await fetch('https://api.weatherapi.com/v1/current.json?key=dacffd3a552541c682a81844232207&q=' + location, {mode: 'cors'})
-	return await response.json()
+	const json =  await response.json()
+	const locationInfos = {}
+	const weatherInfos = {}
+	locationInfos.country = json.location.country
+	locationInfos.time = json.location.localtime
+	locationInfos.name = json.location.name
+	locationInfos.region = json.location.region
+	weatherInfos.cloud = json.current.cloud
+	weatherInfos.condition = json.current.condition.text
+	weatherInfos.feelsLike = json.current.feelslike_c
+	weatherInfos.humidity = json.current.humidity
+	weatherInfos.isDay = Boolean(json.current.is_day)
+	weatherInfos.tempC = json.current.temp_c
+	weatherInfos.windKph = json.current.wind_kph
+	weatherInfos.windDir = json.current.wind_dir
+	return {
+		locationInfos,
+		weatherInfos
+	}
 }
