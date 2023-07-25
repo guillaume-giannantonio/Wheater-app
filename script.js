0290cadd719fa3734f961995129a06ae42810324
guillaume-giannantonio <guillaume.giannantonio@protonmail.com>
2023-07-25 19:06:46 +0200

    add dynamic input

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const searchButton = document.querySelector('button')
+const locationInput = document.querySelector('input')
+
+searchButton.addEventListener('click', async () => {
+	const location = locationInput.value
+	const weatherData = await loadWeatherData(location)
+	console.log(weatherData)
+})
+
 async function loadWeatherData(location) {
-	const response = await fetch('https://api.weatherapi.com/v1/current.json?key=dacffd3a552541c682a81844232207&q=nice', {mode: 'cors'})
-	const json = await response.json()
-	console.log(json)
+	const response = await fetch('https://api.weatherapi.com/v1/current.json?key=dacffd3a552541c682a81844232207&q=' + location, {mode: 'cors'})
+	return await response.json()
 }
-loadWeatherData('test')
